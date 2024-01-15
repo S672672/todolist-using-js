6971ccc564b44f17e4d6dc1487bcdac42c0b19b2
Master <someone@gmail.com>
2024-01-15 18:47:41 +0530

    initial commit

---
+++ store.js
@@ -0,0 +1,21 @@
+ const store = {
+    todos:[
+        {
+            id : "1",
+            title:"complete Task A",
+            completed : false
+        },
+        {
+            id : "2",
+            title:"Go to college",
+            completed : true
+            
+        },
+        {
+            id : "3",
+            title:"drink 3 litres of water",
+            completed : false
+        }
+    ]
+};
+export default store;
